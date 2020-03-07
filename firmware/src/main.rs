9fb842e594b6e3f3a0565d1438cabb29edb457f9
Ryan Summers <ryan.summers@vertigo-designs.com>
2020-03-07 15:35:35 +0100

    Adding functional drawing interface

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -5,8 +5,16 @@ use cortex_m;
 use cortex_m_rt::entry;
 use cortex_m_rt;
 use stm32l4xx_hal as hal;
-
+use ili9341;
+use embedded_graphics::{
+        prelude::*,
+        primitives::Line,
+        style::PrimitiveStyle,
+        pixelcolor::Rgb565,
+};
 use hal::delay::Delay;
+
+use embedded_hal::spi::{Mode, Polarity, Phase};
 
 use hal::prelude::*;
 
@@ -36,7 +44,42 @@ fn main() -> ! {
 
     bl_control.set_high().unwrap();
 
+    // Configure SPI for the TFT-LCD display.
+    let tft_spi_sck = gpiob.pb13.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
+    let tft_spi_miso = gpiob.pb14.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
+    let tft_spi_mosi = gpiob.pb15.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
+
+    let tft_spi = hal::spi::Spi::spi2(
+        dp.SPI2,
+        (tft_spi_sck, tft_spi_miso, tft_spi_mosi),
+        Mode{
+            phase: Phase::CaptureOnFirstTransition,
+            polarity: Polarity::IdleLow,
+        },
+        100.khz(),
+        clocks,
+        &mut rcc.apb1r1,
+    );
+
+    // TODO: User a timer so that clocks are not consumed.
     let mut timer = Delay::new(cp.SYST, clocks);
+
+    // Configure the LCD display.
+    let tft_cs_n = gpiob.pb12.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let tft_d_cx_n = gpiob.pb10.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let tft_rst = gpiob.pb11.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+    let mut display = ili9341::Ili9341::new_spi(
+            tft_spi,
+            tft_cs_n,
+            tft_d_cx_n,
+            tft_rst,
+            &mut timer).unwrap();
+
+    Line::new(Point::new(10, 10), Point::new(20, 20))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 1))
+        .draw(&mut display).unwrap();
+
     loop {
         timer.delay_ms(100 as u32);
         led.set_low().unwrap();
